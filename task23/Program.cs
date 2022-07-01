dfada549ddc4b96634b4c740848edb69bd77b538
Tatyana_K <tomishka_jvk@mail.ru>
2022-07-02 00:06:46 +0400

    ДЗ №3 задача 231

---
+++ task23/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите N =  ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= N; i++)
+{
+    int cube_i = Convert.ToInt32(Math.Pow(i, 3));
+    Console.Write(cube_i);
+    Console.Write(' ');
+}
