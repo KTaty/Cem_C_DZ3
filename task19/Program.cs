ec6cfe28e3aa01aeb6258ffc8af28dc837169dd0
Tatyana_K <tomishka_jvk@mail.ru>
2022-07-02 00:06:10 +0400

    ДЗ №3 задача 19

---
+++ task19/Program.cs
@@ -0,0 +1,29 @@
+﻿int GetReversedNumber(int number)
+{
+    int result = 0;
+    while (number > 0)
+    {
+        int digit = number % 10;
+        result = result * 10 + digit;
+        number = number / 10;
+    }
+    return result;
+}
+
+void GetСheckPalindrom(int num1, int num2)
+{
+    if (num1 == num2)
+    {
+        Console.WriteLine("Число является пaлиндромом");
+    }
+    else
+    {
+        Console.WriteLine("Число не является пaлиндромом");
+    }
+}
+
+
+Console.Write("Введите число для проверки:  ");
+int number_test = Convert.ToInt32(Console.ReadLine());
+int number_revers = GetReversedNumber(number_test);
+GetСheckPalindrom(number_test, number_revers);
